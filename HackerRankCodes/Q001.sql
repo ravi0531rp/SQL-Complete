e7bbc0ec246a355553605756c81ae2b9f3712007
Ravi Prakash <ravi.0531.rp@gmail.com>
2021-09-27 18:24:05 +0530

    NewQuestionsSolved

--- HackerRankCodes/Q001.sql
+++ HackerRankCodes/Q001.sql
@@ -1,5 +1,21 @@
 -- https://www.hackerrank.com/challenges/revising-the-select-query/problem
-
 SELECT *
 FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+
+
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+SELECT NAME 
+FROM CITY
+WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
+
+-- https://www.hackerrank.com/challenges/select-all-sql/problem
+SELECT * FROM CITY;
+
+
+-- https://www.hackerrank.com/challenges/select-by-id/problem
+SELECT *
+FROM CITY
+WHERE ID = 1661;
+
