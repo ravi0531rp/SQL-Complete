1cb650c8fa1723b8de2f65e0313fefb248681103
Ravi Prakash <ravi.0531.rp@gmail.com>
2021-10-02 10:47:46 +0530

    InterviewBitQuery3Added

--- InterviewBitCodes/Q002.sql
+++ InterviewBitCodes/Q002.sql
@@ -27,4 +27,31 @@ movies_cast.movie_id = movies.movie_id
 where movies_cast.role = 'SeanMaguire';
 
 
+-- https://www.interviewbit.com/problems/short-films/
+select 
+movies.movie_title, movies.movie_year, 
+CONCAT(directors.director_first_name,directors.director_last_name) as director_name,
+CONCAT(actors.actor_first_name,actors.actor_last_name) as actor_name,
+movies_cast.role
+from 
+directors
+inner join
+movies_directors
+on 
+movies_directors.director_id = directors.director_id
+inner join 
+movies
+on
+movies.movie_id = movies_directors.movie_id
 
+inner join 
+movies_cast
+on
+movies.movie_id = movies_cast.movie_id
+
+inner join
+actors
+on
+movies_cast.actor_id = actors.actor_id
+order by movies.movie_time
+limit 1;
