618406249d9abb96ba4ce97a985908ed4c20355b
Ravi Prakash <ravi.0531.rp@gmail.com>
2021-10-12 23:49:05 +0530

    W3ResQ1-6

---
+++ W3Resource/Q3.sql
@@ -0,0 +1,11 @@
+-- 1. Write a SQL statement to display all the information of all salesmen.
+SELECT *
+FROM SALESMAN;
+
+-- 2. Write a SQL statement to display a string "This is SQL Exercise, Practice and Solution".
+SELECT 'This is SQL Exercise, Practice and Solution';
+
+-- 6. Write a SQL statement to display specific columns like name and commission for all the salesmen
+SELECT NAME, COMMISSION
+FROM SALESMAN;
+
